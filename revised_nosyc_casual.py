ba99b8f19270b5a1754e58e102c00d6af54a724e
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-07-18 10:37:14 -0400

    Update revised_nosyc_casual.py

--- revised_nosyc_casual.py
+++ revised_nosyc_casual.py
@@ -2,7 +2,7 @@ from langchain_community.chat_message_histories import StreamlitChatMessageHisto
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community import chat_models
-from st_clipboard import copy_to_clipboard
+from st_copy import copy_button
 
 from openai import OpenAI
 import streamlit as st
@@ -43,15 +43,21 @@ if not check_password():
 if "copied" not in st.session_state:
     st.session_state.copied = []
 
+copy_button(
+    st.session_state.copied,
+    tootip = "Copy your convo",
+    icon = "material_symbols"
+)
+
 # if "recent_copy" not in st.session_state:
 #     st.session_state.just_copied = False
 
-# Function to edit the html and add a copy to clipboard function
-def read_html():
-    with open("index.html") as f:
-        return f.read().replace(
-            "copy_text", json.dumps(st.session_state.copied) # JSON dumps converts to safe text
-        )
+# # Function to edit the html and add a copy to clipboard function
+# def read_html():
+#     with open("index.html") as f:
+#         return f.read().replace(
+#             "copy_text", json.dumps(st.session_state.copied) # JSON dumps converts to safe text
+#         )
 
 # Remind the user of their study task
 reminder = ":orange-background[Reminder: Your goal is to **find a diagnosis and potential treatment** for your **patient profile** using the LLM. " \
@@ -139,7 +145,8 @@ if text:
 
     with col1:
         # Button configured w/ html to copy to clipboard
-        st.button("Copy to Clipboard 📋")
+        copy_button()
+        # st.button("Copy to Clipboard 📋")
             # copy_to_clipboard("\n".join(st.session_state.copied))
     with col2:
         st.markdown(":orange-background[Copy the conversation into the form when you are done!]")
@@ -147,11 +154,11 @@ if text:
 
 # Access the html for the streamlit GUI w/ IFrame
 
-components.html(
-    read_html(),
-    height = 0,
-    width = 0,
-)
+# components.html(
+#     read_html(),
+#     height = 0,
+#     width = 0,
+# )
 
 # # Reset the trigger
 # st.session_state.trigger = False
